7dcf13176090f5c8957376b42671735395ac7797
Golam Wasy Arnob <golamwasy@gmail.com>
2026-05-02 20:04:05 +0300

    fixed path issue

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projects/planora/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
